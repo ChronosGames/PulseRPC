3224eb6b59dd4a08bffbee4b8e80bd4320dd9830
yunjing <hqc2010@gmail.com>
2025-04-30 09:16:23 +0800

    chore: 移除不必要的项目文件

--- src/MagicOnion.Abstractions/IgnoreAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace MagicOnion;
-
-/// <summary>
-/// Don't register on MagicOnionEngine.
-/// </summary>
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false,Inherited = false)]
-public class IgnoreAttribute : Attribute
-{
-}
