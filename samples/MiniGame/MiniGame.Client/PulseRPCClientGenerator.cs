9168e20957d3f271479408bb10f657a0c17392c1
huangqc <hqc2010@gmail.com>
2025-05-18 21:13:52 +0800

    feat: 初始化网络管理器，优化网络连接逻辑，添加用户信息获取功能

--- samples/MiniGame/MiniGame.Client/PulseRPCClientGenerator.cs
+++ samples/MiniGame/MiniGame.Client/PulseRPCClientGenerator.cs
@@ -1,8 +1,15 @@
 ﻿using PulseRPC.Client;
 using PulseRPC.Samples.Shared.Messages;
+using MiniGame.Shared;
 
-namespace PulseRPC.Samples.Client;
+namespace MiniGame.Client;
 
+// 基础接口 - 用于源生成
 [PulseClientGeneration(typeof(IAuthStreamingHub))]
 [PulseClientGeneration(typeof(IUserStreamingHub))]
+
+// 实际业务接口 - 继承基础接口
+[PulseClientGeneration(typeof(AuthStreamingHub))]
+[PulseClientGeneration(typeof(GameStreamingHub))]
+[PulseClientGeneration(typeof(INotificationReceiver))]
 public partial class PulseRPCClientGenerator;
