3b041768d5709550db4da73ccb72b2cff0dd7932
huangqc <hqc2010@gmail.com>
2025-05-25 22:33:18 +0800

    chore: 更新Unity版本至2022.3并移除未使用的命名空间

--- samples/ChatApp/ChatApp.Unity/Assets/Scripts/UnityGameClient.cs
+++ samples/ChatApp/ChatApp.Unity/Assets/Scripts/UnityGameClient.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using ChatApp.Shared;
-using Microsoft.Extensions.Logging;
 using PulseRPC;
 using PulseRPC.Client;
 using PulseRPC.Client.Channels;
